ec8265f12f7790454d8f178c1acfb4d9926fddd9
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-15 22:52:10 +0530

    C++ program add

---
+++ Color.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+// Driver Code
+int main() {
+	// B for background Color(light aqua)
+	// A for text color(light purple)
+	system("Color BD");
+
+	// Print any message
+	cout << " WELCOME TO CPPSECRETS!";
+	return 0;
+}
