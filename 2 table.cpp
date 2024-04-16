d3a3805f2b40ecbbc58d89c0b9cfcb669edb69fb
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-16 21:08:37 +0530

    Table

---
+++ 2 table.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int i,j;
+
+
+cout<<"enter number the number"<<endl;
+cin>>j;
+
+
+
+ for ( i = 1; i <= 10; i++)  
+    { 
+	
+	 
+		 cout<< j<<"*"<<i<<"="<<i*j<<endl; 
+    }  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+}
+
+
+
+
+
+
+
+
