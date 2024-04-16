578ecf534c7d67c566cc13b1cc44ff44f2e2fcde
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-16 21:09:33 +0530

    Armstrong no

---
+++ area of circle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float area,r;
+    float PI=3.14;
+
+
+    cout << "Enter the radius of circle:";
+    cin >> r;
+    
+    area=r*r*3.14;
+    cout<<"area of circle is:"<<area;
+    
+    return 0;
+}
