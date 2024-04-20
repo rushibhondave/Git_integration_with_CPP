00f251eacbf31ac94a44369a4926dc361cfbaf8e
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-20 12:39:04 +0530

    Basic Loop C++ Program

---
+++ pattern in 5rows in nested loop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+int a,i,j;
+
+for(i=0;i<3;i++)
+{
+    for(j=0;j<5;j++)
+	{
+		cout<<"*";
+	
+	}
+		cout<<endl;	
+		
+}   
+
+	
+	
+		
+}
