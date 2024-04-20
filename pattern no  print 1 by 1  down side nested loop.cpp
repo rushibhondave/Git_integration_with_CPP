bb6c303f6e697b9dcf649e594881f30538b4da35
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-20 12:39:16 +0530

    Basic Loop C++ Program

---
+++ pattern no  print 1 by 1  down side nested loop.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a,i,j;
+
+cout<<"Enter the number of rows:-";
+cin>>a; 
+
+for(i=1;i<=a;i++)
+{
+   
+   for(j=1;j<=i;j++)
+   {
+   	
+   	cout<<j;
+
+   	
+   }
+   
+   cout<<endl;
+   	
+}
+
+
+
+
+
+}
