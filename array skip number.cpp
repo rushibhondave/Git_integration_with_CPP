d36197a91d99d293bdb31724d4dc0d4db8f57c17
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-21 12:03:53 +0530

    Basic Loop C++ Program

---
+++ array skip number.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main(){
+	
+int i;
+int m[5]={1,2,3,4,5};
+	
+printf("\n");
+
+	
+for(i=0;i<5;i++)
+{
+     printf("%i\t",m[i]);	
+}	
+
+printf("\nskip array is:\n");
+
+	
+for(i=0;i<5;i++)
+{
+		
+if  ( i==3)
+
+
+
+	continue;
+	
+}
+
+for(i=0;i<5;i++)
+{  
+printf("%i\t",m[i]);
+
+}
+	 
+	
+
+	
+	
+	
+	
+}
