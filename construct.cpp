b533e8ebe5160a551e283b1e2a6145b868af5f20
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-27 20:05:36 +0530

    Constructer Code add in

---
+++ construct.cpp
@@ -0,0 +1,79 @@
+// #include <iostream>  
+//using namespace std;  
+//class Employee  
+// {  
+//   public:  
+//        Employee()    
+//        {    
+//            cout<<"Default Constructor Invoked"<<endl;    
+//        }    
+//};  
+//int main(void)   
+//{  
+//    Employee e1; //creating an object of Employee   
+//    Employee e2;   
+//    return 0;  
+//}  
+
+
+
+
+
+//#include <iostream>  
+//using namespace std;  
+//class Student {  
+//   public:  
+//       int id;//data member (also instance variable)      
+//       string name;//data member(also instance variable)      
+//       void insert(int i, string n)    
+//        {    
+//            id = i;    
+//            name = n;    
+//        }    
+//       void display()    
+//        {    
+//            cout<<id<<"  "<<name<<endl;    
+//        }    
+//};  
+//int main(void) {  
+//    Student s1; //creating an object of Student   
+//    Student s2; //creating an object of Student  
+//    s1.insert(201, "Sonoo");    
+//    s2.insert(202, "Nakul");    
+//    s1.display();    
+//    s2.display();  
+//    return 0;  
+//}  
+
+
+//
+//#include <iostream>
+//using namespace std;
+//class Employee {
+//   public:
+//       int id;//data member (also instance variable)    
+//       string name;//data member(also instance variable)
+//       float salary;
+//       Employee(int i, string n, float s)  
+//        {  
+//            id = i;  
+//            name = n;  
+//            salary = s;
+//        }  
+//       void display()  
+//        {  
+//            cout<<id<<"  "<<name<<"  "<<salary<<endl;  
+//        }  
+//};
+//int main(void) {
+//    Employee e1 =Employee(101, "Sonoo", 890000); //creating an object of Employee 
+//    Employee e2=Employee(102, "Nakul", 59000); 
+//    e1.display();  
+//    e2.display();  
+//    return 0;
+//}
+
+
+
+
+
