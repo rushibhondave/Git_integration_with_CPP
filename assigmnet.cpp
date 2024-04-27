225747de807b74125f66ddf613e02b3c81f86d60
Rushi <rushikesh.21910052@viit.ac.in>
2024-04-28 00:08:52 +0530

    Inhertance Code add in C++

---
+++ assigmnet.cpp
@@ -0,0 +1,20 @@
+asssignmanet
+
+sigle inheratace 
+person base
+id name
+employe dervied;
+name id salaray address;
+
+multiple inhertance
+hospital
+name adress	
+patieny 
+name id
+bill
+bill date
+bill amount 
+
+
+2)empolye sa laray ;
+3)hospital bill;
