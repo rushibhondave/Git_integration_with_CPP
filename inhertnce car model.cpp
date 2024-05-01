af073ee59d067a826fd7d474812beefbb9d863c7
Rushi <rushikesh.21910052@viit.ac.in>
2024-05-02 00:43:42 +0530

    Inhertance program in c++

---
+++ inhertnce car model.cpp
@@ -0,0 +1,32 @@
+#include <iostream>  
+using namespace std;
+
+
+class vechical
+{
+	protected:
+		int model_id;
+		    int model_price;
+		       string model_name;
+		       
+	public:void display()
+	{
+		
+		cout<<"enter the model_id:";
+		cin>>model_id;
+		
+		cout<<"enter the model_price:";
+		cin>>model_price;
+		
+		cout<<"enter the model_name:";
+		cin>>model_name;
+		
+	}
+		
+		
+		
+	
+	
+	
+	
+}
