77b7bdea1ca4ced91902c3042d19a6fea21c5005
Rushi <rushikesh.21910052@viit.ac.in>
2024-05-02 00:42:22 +0530

    Genric program in c#

---
+++ templates.cpp
@@ -0,0 +1,195 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//template<class T>
+//
+//void show(T a,T b)
+//{
+//	
+//	
+//	cout<<"\na::"<<a<<"\tb::"<<b;
+//	
+//}
+//
+//
+//
+//
+//
+//main()
+//{
+//	
+//  int p=10,q=15;
+//  show(p,q);
+//  
+// float m=2.1,n=2.5;
+//  show(m,n);
+//  
+//  
+//  int x='2',y='4';
+//  show(x,y);	
+//	
+//	
+//	
+//	
+//}
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+//#include<iostream>
+//
+//using namespace std;
+//
+//template <class T>
+// 
+// class A
+// {
+// 	
+// 	 T a;
+// 	 T b;
+// 	 
+// 	 public:
+// 	 	A(T x,T y)
+// 	 	{
+// 	 		a=x;
+// 	 		b=y;
+// 	 	
+//		  }
+//     
+//     
+//	 
+//	 void display()
+//	 {
+//	    cout<<"Values of a & b::\n"<<"A::"<<a<<"\tB::"<<b;	
+//	 	
+//	 	
+//	 	
+//		 }	
+// };
+// 
+// 
+// 
+//main()
+//
+//{
+//	
+//	A<int> d(4,6);
+//	d.display();
+//	
+//	
+//	
+//	
+//}
+
+
+//
+//#include<iostream>
+//
+//using namespace std;
+//
+//template <class T>
+// 
+// 
+//class S
+//{
+//	int temp=0;
+//  	
+//  	 
+//  
+//	 public:
+//	 	
+//	    void swap(T a,T b)
+//	  {	 
+//		
+//	  
+//	   temp=a;
+//	   a=b;
+//	   b=temp;
+//	 
+//	 
+//	  cout<<" swapping Values of a & b::\n"<<"A::"<<a<<"\tB::"<<b;	
+//	 	
+//	 }	
+//	
+//	
+//};
+//
+//
+//
+//main()
+//{
+//
+//	
+// S<int> d;
+//
+// d.swap(4,6);
+//
+//
+//}
+////
+//
+//
+//
+//#include<iostream>
+//
+//using namespace std;
+//
+//template <class T>
+// 
+// 
+//class S
+//{
+//  
+//	 public:
+//	 	
+//	    void swap(T a,T b)
+//	  {	 
+//		
+//	  
+//		 a=a+b;   
+//	     b=a-b;   
+//	     a=a-b;
+//	 
+//	 
+//	  cout<<" swapping Values of a & b::\n"<<"A::"<<a<<"\tB::"<<b;	
+//	 	
+//	 }	
+//	
+//	
+//};
+//
+//
+//
+//main()
+//{
+//
+//
+//int a,b,c;
+//
+//cout<<"Enter the Swapping two number::";
+//cin>>a>>b;
+//	
+// S<int> d;
+//
+// d.swap(a,b);
+//	
+//}
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+  
