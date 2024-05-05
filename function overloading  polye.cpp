cc572842ac548e2f83fd3aba2566fe2952d5975b
Rushi <rushikesh.21910052@viit.ac.in>
2024-05-05 21:19:29 +0530

    function overridding   polye

---
+++ function overloading  polye.cpp
@@ -0,0 +1,77 @@
+    //#include <iostream>  
+//using namespace std;
+//
+//void product(int x,int y)
+//{
+//	cout<<"Multiplication::"<<x*y;
+//	
+//	
+//}
+//
+//void product(int x,double y)
+//{
+//	cout<<"Multiplication::"<<x*y;
+//	
+//	
+//}
+//
+//
+//void product(double x,int y)
+//{
+//	cout<<"Multiplication::"<<x+y;
+//	
+//	
+//}
+//
+//void product(double x,double y)
+//{
+//	cout<<"Multiplication::"<<x+y;
+//	
+//	
+//}
+//
+//
+//
+//main()
+//{
+//	 
+//	product(3,3);
+//	product(3,3.3);
+//	product(4.3,5);
+//	product(4.4,4.4);
+//	
+//	
+//	
+//	
+//	
+//}
+
+//
+//#include <iostream>  
+//using namespace std;
+//
+//int reactangle(int l,int b)
+//{
+//	return l*b;
+//	
+//}
+//
+//
+//float  area(float r)
+//{
+//	
+//	return 3.14*r*r;
+//	
+//}
+//
+//
+//
+//main()
+//{
+//	reactangle(3,4);
+//	cout<<"\nArea of reactangle::"<<reactangle(3,4);
+//	
+//	area(2.0);
+//	cout<<"\nArea of circle::"<<area(2.0);
+//	
+//}
