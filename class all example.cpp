87adb0863f24d2068bcab368288b5558c69f6def
Rushi <rushikesh.21910052@viit.ac.in>
2024-05-19 10:28:16 +0530

    C++ Program

---
+++ class all example.cpp
@@ -0,0 +1,124 @@
+////#include <iostream>  
+////using namespace std;
+////
+////class student
+////{
+////	private:
+////		int roll_no;
+////		   string name;
+////		   
+////	public:
+////		
+////		void get()
+////		{
+////			cout<<"Enter the student name::";
+////			cin>>name;
+////			
+////			cout<<"\nEnter the student roll no::";
+////			cin>>roll_no;
+////			
+////		}
+////		
+////		void put()
+////		{
+////			cout<<"\nName of student::"<<name;
+////			cout<<"\nstudent roll no::"<<roll_no;
+////			
+////			
+////			
+////		}
+////	
+////};
+////
+////
+////
+////int main()
+////{
+////	student s1;
+////	s1.get();
+////	s1.put();
+////	
+////	
+////	
+////}
+////
+////
+//
+//
+//
+//
+//#include <iostream>  
+//using namespace std;
+//
+//
+//class student
+//{ 
+//    private:
+//    	int roll_no;
+//    	 int phone_no;;
+//    	   string adress,name;
+//    	
+//    
+//    public:
+//     
+//	    void get()
+//		{
+//		       cout<<"Enter the student name::";
+//			   cin>>name;
+//			   cout<<"\n";
+//			   cout<<"\nEnter the roll no::";
+//			   cin>>roll_no;
+//			   cout<<"\n";
+//			   cout<<"\nEnter the phone no::";
+//			   cin>>phone_no;
+//			   cout<<"\n";
+//			   cout<<"\nEnter the adress::";
+//			   cin>>adress;
+//			   
+//		  
+//		}
+//		
+//		void put()
+//		{
+//		     
+//			   cout<<"\nstudent name::"<<name;
+//			       cout<<"\n";
+//				cout<<"\nstudent roll_no::"<<roll_no;
+//			     	   cout<<"\n";
+//					cout<<"\nstudent phone_no::"<<phone_no;
+//					      cout<<"\n";
+//						cout<<"\nstudent adress::"<<"\n"<<adress;
+//					         cout<<"\n";
+//			}
+//			
+//				void putdata()
+//		{
+//		     
+//			   cout<<"\nstudent name::"<<name;
+//			       cout<<"\n";
+//				cout<<"\nstudent roll_no::"<<roll_no;
+//			     	   cout<<"\n";
+//					cout<<"\nstudent phone_no::"<<phone_no;
+//					      cout<<"\n";
+//						cout<<"\nstudent adress::"<<"\n"<<adress;
+//					         cout<<"\n";
+//			}	
+//    		
+//    	
+//
+//};
+//
+//
+//int main()
+//{
+//  student s1;
+//  student s2;
+//  s1.get();
+//  s1.put();
+//  s2.get();
+//  s2.putdata();	
+//
+//}
+
+
+
