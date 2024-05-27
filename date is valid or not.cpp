35ab66b734e0dfa4e85c8531b8f492f8df12ab58
Rushi <rushikesh.21910052@viit.ac.in>
2024-05-27 23:54:37 +0530

    date is valid or not

---
+++ date is valid or not.cpp
@@ -0,0 +1,32 @@
+#include<Stdio.h>
+ 
+ int main()
+ {
+ 	int d,m,y;
+ 	
+ 	
+ printf("Enter the date :\n");
+ scanf("%i",&d);
+ 
+ printf("Enter the month :\n");
+ scanf("%i",&d);
+ 
+ printf("Enter the date :\n");
+ scanf("%i",&y);
+ 
+ 
+ if(d>==1 && d<==31)
+ 
+ printf("date is correct/n");
+ 
+ else if ()
+  
+
+
+else 
+{
+	printf("invalid selection");
+}
+
+
+}
