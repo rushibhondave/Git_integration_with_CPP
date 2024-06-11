5bbc1252eca5920c914bfb8656c62576a328238f
Rushi <rushikesh.21910052@viit.ac.in>
2024-06-11 23:35:50 +0530

    200~nested looop in for loop

--- String_DSA/Reverse a String in C++.cpp
+++ String_DSA/Reverse a String in C++.cpp
@@ -5,24 +5,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//int main()
+//{
+//   string rev1="Rushikesh is boy",ram;
+//   char rev;
+//   reverse(rev1.begin(),rev1.end());
+//   cout<<"Reverse using function :"<<rev1<<endl;
+//   
+//   //Reverse without using function C++
+//   int l=rev1.length();
+//   for(int i=0;i<l;i++)
+//   {
+//   	  rev=rev1[i];
+//   	  rev1[i]=rev1[l-1-i];
+//      rev1[l-1-i]=rev;
+//      
+//   }
+//    cout<<"Reverse without using function :"<<rev1;
+//  
+//
+//}
+
 int main()
 {
-   string rev1="Rushikesh is boy",ram;
-   char rev;
-   reverse(rev1.begin(),rev1.end());
-   cout<<"Reverse using function :"<<rev1<<endl;
-   
-   //Reverse without using function C++
-   int l=rev1.length();
-   for(int i=0;i<l;i++)
-   {
-   	  rev=rev1[i];
-   	  rev1[i]=rev1[l-1-i];
-      rev1[l-1-i]=rev;
-      
-   }
-    cout<<"Reverse without using function :"<<rev1;
+   string str="Rushikesh is boy",ram;
+   int n=256;
+   int count[n]={0};
+        
+        for(int i=0;i<str.length();i++)
+        {
+            count[ str [i]]++;
+           
+        }
+        
+           for(int i=0;i<n;i++)
+        {
+            if(count[i]>1)
+            {
+            	cout<<(char)i<<count[i]<<endl;
+            	
+			}
+           
+        }
+        
+        
+      cout<<count<<endl;
   
 
 }
-
---
+++ nested looop in for loop2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	
+int i,j;
+
+for(i=0;i<1;i++)
+{
+	
+	for(j=0;j<10;j++)
+	{
+	
+		
+	 cout<<"i j=\t"<<i<<"\t"<<j<<endl;	
+		
+		
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
