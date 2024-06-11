823ed4b564576cf05a9f95a203f4c33fba0a4e25
Rushi <rushikesh.21910052@viit.ac.in>
2024-06-11 23:38:28 +0530

    Patter quetion using nested loop

---
+++ pattern no  print  down side nested loop.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a,i,j;
+
+cout<<"Enter the number of rows:-";
+cin>>a; 
+
+for(i=a;i>=1;i--)
+{
+	
+	for(j=5;j>=i;j--)
+	{
+	
+	cout<<j;	
+		
+	}
+	cout<<endl;
+}
+
+
+
+
+
+
+
+
+}
