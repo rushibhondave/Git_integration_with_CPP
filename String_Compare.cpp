0ede2f9f86408bb6aec302949a2befad4b60a1cf
Rushi <rushikesh.21910052@viit.ac.in>
2024-06-12 23:11:56 +0530

    String compare

--- String_Compare.cpp
+++ String_Compare.cpp
@@ -6,6 +6,31 @@ int main()
 { 
      string str="Rushikesh";
 	 char str2[]="Rushi";
+	 int flag=0;
+     int len1=strlen(str2);
+   	int len=str.length();
+	
+	for(int i=0;i<len;i++)
+	{
+		if(str[i]==str2[i])
+		{
+			flag++;
+		}
+		else
+		{
+		    flag=0;
+		    break;
+		}
+	}
+	 
+	 if(flag>1)
+	 {
+	 	cout<<"String is same";
+	 }
+	 else
+	 {
+	 cout<<"String is not same";
+	 }
 	
 	
 }
